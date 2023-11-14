9cd3581afefe15a38cbc7b951887ae1799fe1270
Philip Mørch <mrphil2105@gmail.com>
2023-11-14 23:54:05 +0100

    feat: add initial Vim options

---
+++ lua/options.lua
@@ -0,0 +1,16 @@
+vim.wo.number = true -- Line numbers
+
+vim.o.hlsearch = false -- Highlight on search
+
+vim.o.expandtab = true -- Spaces for indentation
+vim.o.smartindent = true -- Automatic identation
+vim.o.tabstop = 4 -- Four space tab
+vim.o.shiftwidth = 4 -- Indent four spaces
+
+vim.o.undofile = true -- Save undo history
+
+vim.wo.signcolumn = "yes" -- Show sign column
+
+vim.o.updatetime = 1000 -- Swap file delay
+
+vim.o.termguicolors = true -- 24-bit colors
