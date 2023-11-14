349027011f59ec5eded2eeaf8cce05cc87755e95
Philip Mørch <mrphil2105@gmail.com>
2023-11-14 23:50:13 +0100

    feat: set up LSP and Mason

--- init.lua
+++ init.lua
@@ -1,18 +1,17 @@
 vim.g.mapleader = " "
 
--- Lazy Plugin Manager
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({})
+require("lazy").setup("plugins")
+require("lsp")
---
+++ lua/lsp.lua
@@ -0,0 +1,28 @@
+local servers = {
+    clangd = {},
+    lua_ls = {
+        Lua = {
+            workspace = { checkThirdParty = false },
+            diagnostics = {
+                globals = { "vim" }
+            },
+            telemetry = { enable = false },
+        },
+    },
+}
+
+require("mason").setup()
+local mason_lspconfig = require("mason-lspconfig")
+
+mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers)
+}
+
+mason_lspconfig.setup_handlers {
+    function(server_name)
+        require("lspconfig")[server_name].setup {
+            settings = servers[server_name],
+            filetypes = (servers[server_name] or {}).filetypes
+        }
+    end
+}
