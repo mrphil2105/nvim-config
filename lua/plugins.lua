e7f3e2e2ed75d79b03d38fa0c9480b8552ceb6e5
Philip Mørch <mrphil2105@gmail.com>
2023-11-19 19:31:41 +0100

    feat: set up Telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -32,6 +32,13 @@ return {
         }
     },
     {
+        "nvim-telescope/telescope.nvim",
+        branch = "0.1.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        }
+    },
+    {
         "akinsho/toggleterm.nvim",
         opts = {}
     },
