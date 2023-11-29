9315e70345a22de9822ab363dab81d2e3930cb85
Philip Mørch <mrphil2105@gmail.com>
2023-11-29 15:14:26 +0100

    feat: set up ESLint

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -20,10 +20,21 @@ mason_lspconfig.setup {
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+local on_attach = function(client, buffer)
+    if client.name == "eslint" then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = buffer,
+            command = "EslintFixAll"
+        })
+        vim.cmd("EslintFixAll")
+    end
+end
+
 mason_lspconfig.setup_handlers {
     function(server_name)
         require("lspconfig")[server_name].setup {
             capabilities = capabilities,
+            on_attach = on_attach,
             settings = servers[server_name],
             filetypes = (servers[server_name] or {}).filetypes
         }
