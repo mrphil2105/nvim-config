966207ee069e7ddf20fbbb5e050fdcd2ec5ca7c8
Philip Mørch <mrphil2105@gmail.com>
2023-12-08 20:25:11 +0100

    feat: add Lazygit to toggleterm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,18 @@
 return {
     "akinsho/toggleterm.nvim",
-    opts = {
-        direction = "float",
-        open_mapping = [[<c-\>]],
-    },
+    config = function()
+        require("toggleterm").setup {
+            direction = "float",
+            open_mapping = [[<c-\>]],
+        }
+
+        local Terminal = require("toggleterm.terminal").Terminal
+        local lazygit = Terminal:new { cmd = "lazygit", hidden = true }
+
+        local lazygit_toggle = function()
+            lazygit:toggle()
+        end
+
+        vim.keymap.set("n", "<leader>g", lazygit_toggle, { desc = "Toggle Lazygit" })
+    end,
 }
