0674186f441f0be82cb09f15b56dcd556a6fc3e0
Philip Mørch <mrphil2105@gmail.com>
2024-01-26 12:55:24 +0100

    feat: set up Chrome DAP

--- lua/plugins/dap/nodejs.lua
+++ lua/plugins/dap/nodejs.lua
@@ -19,6 +19,38 @@ local function register_keymaps(dap, configs)
     end, { desc = "Restart All" })
 end
 
+local function configure_node(app, options, configs)
+    local js_file = vim.fn.getcwd() .. "/" .. options.main
+    local config = {
+        type = "pwa-node",
+        request = "launch",
+        name = app,
+        cwd = "${workspaceFolder}",
+        program = js_file,
+        args = options.args,
+        env = options.env,
+        sourceMaps = true,
+        protocol = "inspector",
+        console = "integratedTerminal",
+        skipFiles = { "<node_internals>/**", "**/node_modules/**" },
+    }
+    table.insert(configs, config)
+end
+
+local function configure_chrome(app, options, configs)
+    local config = {
+        type = "pwa-chrome",
+        request = "launch",
+        name = app,
+        webRoot = "${workspaceFolder}",
+        url = options.main,
+        sourceMaps = true,
+        protocol = "inspector",
+        userDataDir = "${workspaceFolder}/.chromium-user-data",
+    }
+    table.insert(configs, config)
+end
+
 function M.setup()
     if enabled() == 0 then return end
 
@@ -26,45 +58,43 @@ function M.setup()
     local toml = require("toml")
     local success, run_config = pcall(toml.decodeFromFile, run_file)
 
-    dap.adapters["pwa-node"] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-            command = "node",
-            args = {
-                vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
-                "${port}",
+    local dap_types = { "pwa-node", "pwa-chrome" }
+
+    for _, dap_type in ipairs(dap_types) do
+        dap.adapters[dap_type] = {
+            type = "server",
+            host = "localhost",
+            port = "${port}",
+            executable = {
+                command = "node",
+                args = {
+                    "--inspect",
+                    vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js",
+                    "${port}",
+                },
             },
-        },
-    }
-
-    if success then
-        local configs = {}
-
-        for app, options in pairs(run_config) do
-            local js_file = vim.fn.getcwd() .. "/" .. options.main
-            local config = {
-                type = "pwa-node",
-                request = "launch",
-                name = app,
-                cwd = "${workspaceFolder}",
-                program = js_file,
-                args = options.args,
-                env = options.env,
-                sourceMaps = true,
-                protocol = "inspector",
-                console = "integratedTerminal",
-                skipFiles = { "<node_internals>/**", "node_modules/**" },
-            }
-            table.insert(configs, config)
-        end
-
-        register_keymaps(dap, configs)
-        dap.configurations.typescript = configs
-    else
-        vim.api.nvim_err_writeln("Node.js DAP Failure: " .. vim.inspect(run_config))
+        }
     end
+
+    if not success then
+        vim.api.nvim_err_writeln("Node.js DAP Failure: " .. vim.inspect(run_config))
+        return
+    end
+
+    local configs_node = {}
+    local configs_chrome = {}
+
+    for app, options in pairs(run_config) do
+        if string.match(options.main, "^https?://") then
+            configure_chrome(app, options, configs_chrome)
+        else
+            configure_node(app, options, configs_node)
+        end
+    end
+
+    register_keymaps(dap, configs_node)
+    dap.configurations.typescript = configs_node
+    dap.configurations.typescriptreact = configs_chrome
 end
 
 return M
