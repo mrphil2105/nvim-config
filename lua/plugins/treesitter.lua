5e9b133612b373e7c8c373bfaf7e581564ffe0ed
Philip Mørch <mrphil2105@gmail.com>
2024-02-15 18:59:30 +0100

    feat: remove parameter swap

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -83,15 +83,6 @@ return {
                         ["[]"] = "@class.outer",
                     },
                 },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>a"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>A"] = "@parameter.inner",
-                    },
-                },
             },
         }
     end,
