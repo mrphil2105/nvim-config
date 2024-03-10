26b5c89550eaa790750fd9d46dd50e93abd223d7
Philip Mørch <mrphil2105@gmail.com>
2024-03-10 21:59:12 +0100

    feat: set up multi-project Rust DAP
    This also adds asynchronous compiling of the Rust projects, before running the debugger.

--- lua/plugins/dap/rust.lua
+++ lua/plugins/dap/rust.lua
@@ -1,20 +1,81 @@
 local M = {}
 
-local cargo_file = vim.fn.getcwd() .. "/Cargo.toml"
+local cargo_file_name = "Cargo.toml"
 
-local function enabled() return vim.fn.filereadable(cargo_file) == 1 end
+local function get_cargo_files()
+    local utils = require("utils")
+
+    local cwd = vim.fn.getcwd()
+    local cwd_content = vim.split(vim.fn.glob(cwd .. "/*"), "\n", { trimempty = true })
+    local cargo_files = {}
+
+    local cargo_file = cwd .. "/" .. cargo_file_name
+
+    if utils.file_exists(cargo_file) then table.insert(cargo_files, cargo_file) end
+
+    for _, path in ipairs(cwd_content) do
+        cargo_file = path .. "/" .. cargo_file_name
+        local main_file = path .. "/src/main.rs"
+
+        if vim.fn.isdirectory(path) and utils.file_exists(cargo_file) and utils.file_exists(main_file) then
+            table.insert(cargo_files, cargo_file)
+        end
+    end
+
+    return cargo_files
+end
+
+local is_building = false
+
+local function register_build_keymap(cargo_files)
+    local dap = require("dap")
+    local utils = require("utils")
+
+    vim.schedule(function()
+        vim.keymap.set("n", "<leader>bc", function()
+            if is_building then return end
+            is_building = true
+            vim.notify("Building Rust projects...")
+
+            local success_count = 0
+            local finished_count = 0
+            local project_count = utils.table_length(cargo_files)
+
+            for _, cargo_file in ipairs(cargo_files) do
+                local working_dir = vim.fs.dirname(cargo_file)
+                local on_exit = function(_, exit_code)
+                    if exit_code == 0 then
+                        success_count = success_count + 1
+
+                        if success_count == project_count then
+                            vim.notify("Build finished.")
+                            dap.continue()
+                        end
+                    else
+                        vim.notify("Cargo build failed with code: " .. exit_code)
+                    end
+
+                    finished_count = finished_count + 1
+                    if finished_count == project_count then is_building = false end
+                end
+
+                local opts = {
+                    cwd = working_dir,
+                    on_exit = on_exit,
+                }
+                vim.fn.jobstart({ "cargo", "build" }, opts)
+            end
+        end, { desc = "Continue Execution" })
+    end)
+end
 
 function M.setup()
-    if not enabled() then return end
+    local cargo_files = get_cargo_files()
+
+    if next(cargo_files) == nil then return end
 
     local dap = require("dap")
     local toml = require("toml")
-    local success, cargo_manifest = pcall(toml.decodeFromFile, cargo_file)
-
-    if not success then
-        vim.api.nvim_err_writeln("Rust DAP Failure: " .. vim.inspect(cargo_manifest))
-        return
-    end
 
     dap.adapters.codelldb = {
         type = "server",
@@ -25,20 +86,30 @@ function M.setup()
         },
     }
 
-    dap.configurations.rust = {
-        {
+    local configs = {}
+
+    for _, cargo_file in ipairs(cargo_files) do
+        local success, cargo_manifest = pcall(toml.decodeFromFile, cargo_file)
+
+        if not success then
+            vim.api.nvim_err_writeln("Failed to decode Rust manifest: " .. vim.inspect(cargo_manifest))
+            return
+        end
+
+        local working_dir = vim.fs.dirname(cargo_file)
+        local config = {
             type = "codelldb",
             request = "launch",
-            name = "Debug application",
-            cwd = "${workspaceFolder}",
-            program = function()
-                local output = vim.fn.system("cargo build")
-                if vim.v.shell_error ~= 0 then vim.notify("Cargo build failed: " .. output) end
-                return vim.fn.getcwd() .. "/target/debug/" .. cargo_manifest.package.name
-            end,
+            name = cargo_manifest.package.name,
+            cwd = working_dir,
+            program = working_dir .. "/target/debug/" .. cargo_manifest.package.name,
             stopOnEntry = false,
-        },
-    }
+        }
+        table.insert(configs, config)
+    end
+
+    register_build_keymap(cargo_files)
+    dap.configurations.rust = configs
 end
 
 return M
--- lua/utils.lua
+++ lua/utils.lua
@@ -1,5 +1,15 @@
 local M = {}
 
+function M.table_length(tab)
+    local count = 0
+    for _ in pairs(tab) do
+        count = count + 1
+    end
+    return count
+end
+
 function M.endswith(str, suf) return string.sub(str, -#suf) == suf end
+
+function M.file_exists(path) return vim.fn.filereadable(path) == 1 end
 
 return M
