954c9d2a95df8956f99f27bd7756bb93ba67a2af
Philip Mørch <mrphil2105@gmail.com>
2024-03-30 18:01:48 +0100

    chore: add nvim-nio dependency

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,7 +1,7 @@
 return {
     "mfussenegger/nvim-dap",
     dependencies = {
-        "rcarriga/nvim-dap-ui",
+        { "rcarriga/nvim-dap-ui", dependencies = { "nvim-neotest/nvim-nio" } },
         "nvim-telescope/telescope-dap.nvim",
     },
     config = function()
