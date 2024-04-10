19898039e8e508053fb323d55b25b020649790cd
Philip Mørch <mrphil2105@gmail.com>
2024-04-10 15:34:41 +0200

    feat: add path_combine util function

--- lua/utils.lua
+++ lua/utils.lua
@@ -12,4 +12,21 @@ function M.endswith(str, suf) return string.sub(str, -#suf) == suf end
 
 function M.file_exists(path) return vim.fn.filereadable(path) == 1 end
 
+---Combines a list of path segments to a full path.
+---@param ... string A list of path segments to combine.
+---@return string
+function M.path_combine(...)
+    local parts = { ... }
+    local len = M.table_length(parts)
+    local path = ""
+    for i = 1, len do
+        local part = parts[i]
+        if i ~= 1 and part:sub(1, 1) == "/" then part = part:sub(2) end
+
+        path = path .. part
+        if i ~= len and path:sub(-1) ~= "/" then path = path .. "/" end
+    end
+    return path
+end
+
 return M
