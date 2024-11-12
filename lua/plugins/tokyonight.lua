677dcbaaec0ae28704371cee8a4e9abeefafb8ad
Philip Mørch <mrphil2105@gmail.com>
2024-11-12 12:40:36 +0100

    feat: brighten gutter foreground color

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -2,7 +2,9 @@ return {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
-        require("tokyonight").setup()
+        require("tokyonight").setup {
+            on_colors = function(colors) colors.fg_gutter = "#555f8d" end,
+        }
         vim.cmd([[colorscheme tokyonight-night]])
     end,
 }
