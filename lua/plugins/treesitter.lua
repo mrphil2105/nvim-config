4215a6898f4fb4d8cf86f0bf6bfa6dd32ad2328b
Philip Mørch <mrphil2105@gmail.com>
2024-11-22 18:12:52 +0100

    feat: add Erlang to Treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -23,6 +23,7 @@ return {
                 "python",
                 "go",
                 "proto",
+                "erlang",
                 "html",
                 "css",
                 "scss",
