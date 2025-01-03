199d8650e3c74954c57ef646ce95bdbd76c719fa
Philip Mørch <mrphil2105@gmail.com>
2025-01-03 17:23:02 +0100

    fix: tab selects CMP completion items

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -35,18 +35,14 @@ return {
                     end
                 end),
                 ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.locally_jumpable(1) then
+                    if luasnip.locally_jumpable(1) then
                         luasnip.jump(1)
                     else
                         fallback()
                     end
                 end, { "i", "s" }),
                 ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
+                    if luasnip.locally_jumpable(-1) then
                         luasnip.jump(-1)
                     else
                         fallback()
