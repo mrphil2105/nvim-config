cb14acf72af5e9ce6641cbdddef528b391607ea4
Philip Mørch <mrphil2105@gmail.com>
2025-03-07 14:25:19 +0100

    feat: wait 500 ms before restarting debug sessions

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -57,7 +57,9 @@ return {
                     return
                 end
                 vim.notify("Restarting session: " .. session.config.name)
-                dap.restart(session.config)
+                dap.set_session(session)
+                dap.terminate()
+                vim.defer_fn(function() dap.run(session.config, { new = true }) end, 500)
             end)()
         end, { desc = "Restart Session" })
     end,
