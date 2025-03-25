3fcd1613696e8a78fc9b10429180fbd0bceddb67
Philip Mørch <mrphil2105@gmail.com>
2025-03-25 16:37:20 +0100

    fix: remove on_attach on Metals config

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -4,11 +4,6 @@ return {
         "nvim-lua/plenary.nvim",
     },
     ft = { "scala", "sbt" },
-    opts = function()
-        local metals_config = require("metals").bare_config()
-        metals_config.on_attach = require("plugins.lsp.attach")
-        return metals_config
-    end,
     config = function(self, metals_config)
         local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
         vim.api.nvim_create_autocmd("FileType", {
