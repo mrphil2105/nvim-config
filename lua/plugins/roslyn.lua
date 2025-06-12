1916ec7c8e38166052cf52030eeeb9c0f9d94a2d
Philip Mørch <mrphil2105@gmail.com>
2025-06-12 16:55:18 +0200

    refactor: use environment variable for rzls path

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -12,24 +12,21 @@ return {
         local capabilities = vim.lsp.protocol.make_client_capabilities()
         capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
         require("roslyn").setup {
+            exe = "Microsoft.CodeAnalysis.LanguageServer",
             args = {
                 "--stdio",
                 "--logLevel=Information",
                 "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
                 "--razorSourceGenerator=" .. vim.fs.joinpath(
-                    vim.fn.stdpath("data") --[[@as string]],
-                    "mason",
-                    "packages",
-                    "roslyn",
-                    "libexec",
+                    os.getenv("RZLS_PATH") --[[@as string]],
+                    "lib",
+                    "rzls",
                     "Microsoft.CodeAnalysis.Razor.Compiler.dll"
                 ),
                 "--razorDesignTimePath=" .. vim.fs.joinpath(
-                    vim.fn.stdpath("data") --[[@as string]],
-                    "mason",
-                    "packages",
+                    os.getenv("RZLS_PATH") --[[@as string]],
+                    "lib",
                     "rzls",
-                    "libexec",
                     "Targets",
                     "Microsoft.NET.Sdk.Razor.DesignTime.targets"
                 ),
