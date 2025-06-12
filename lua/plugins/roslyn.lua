8d69fa094ab8582bf9e36c4ac946235bee36b04d
Philip Mørch <mrphil2105@gmail.com>
2025-06-12 18:43:47 +0200

    feat: update roslyn.nvim and rzls.nvim config

--- lua/plugins/roslyn.lua
+++ lua/plugins/roslyn.lua
@@ -3,41 +3,38 @@ return {
     dependencies = {
         {
             "tris203/rzls.nvim",
-            opts = {},
+            config = function()
+                local rzls_base_path = vim.fs.joinpath(os.getenv("RZLS_PATH"), "lib", "rzls")
+                local cmd = {
+                    "Microsoft.CodeAnalysis.LanguageServer",
+                    "--stdio",
+                    "--logLevel=Information",
+                    "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
+                    "--razorSourceGenerator="
+                        .. vim.fs.joinpath(rzls_base_path, "Microsoft.CodeAnalysis.Razor.Compiler.dll"),
+                    "--razorDesignTimePath="
+                        .. vim.fs.joinpath(rzls_base_path, "Targets", "Microsoft.NET.Sdk.Razor.DesignTime.targets"),
+                }
+                local capabilities = vim.lsp.protocol.make_client_capabilities()
+                capabilities =
+                    vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
+                vim.lsp.config("roslyn", {
+                    cmd = cmd,
+                    handlers = require("rzls.roslyn_handlers"),
+                    capabilities = capabilities,
+                })
+                require("rzls").setup {
+                    path = "rzls",
+                    capabilities = capabilities,
+                    -- Handled by an autocommand in attach.lua.
+                    on_attach = function() end,
+                }
+            end,
         },
         "jlcrochet/vim-razor",
     },
     ft = { "cs", "razor" },
-    config = function()
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
-        require("roslyn").setup {
-            exe = "Microsoft.CodeAnalysis.LanguageServer",
-            args = {
-                "--stdio",
-                "--logLevel=Information",
-                "--extensionLogDirectory=" .. vim.fs.dirname(vim.lsp.get_log_path()),
-                "--razorSourceGenerator=" .. vim.fs.joinpath(
-                    os.getenv("RZLS_PATH") --[[@as string]],
-                    "lib",
-                    "rzls",
-                    "Microsoft.CodeAnalysis.Razor.Compiler.dll"
-                ),
-                "--razorDesignTimePath=" .. vim.fs.joinpath(
-                    os.getenv("RZLS_PATH") --[[@as string]],
-                    "lib",
-                    "rzls",
-                    "Targets",
-                    "Microsoft.NET.Sdk.Razor.DesignTime.targets"
-                ),
-            },
-            ---@diagnostic disable-next-line: missing-fields
-            config = {
-                handlers = require("rzls.roslyn_handlers"),
-                capabilities = capabilities,
-            },
-        }
-    end,
+    config = true,
     init = function()
         vim.filetype.add {
             extension = {
