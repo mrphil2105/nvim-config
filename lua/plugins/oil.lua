625a55c5377bcab5eaefb1e392de76fab190a3b5
Philip Mørch <mrphil2105@gmail.com>
2025-11-10 16:04:07 +0100

    fix: restore borders for Oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,18 +1,34 @@
 return {
     "stevearc/oil.nvim",
+    lazy = false,
     dependencies = {
         "nvim-tree/nvim-web-devicons",
     },
-    config = function()
-        local oil = require("oil")
-
-        oil.setup {
-            float = {
-                padding = 4,
-                border = "single",
-            },
-        }
-
-        vim.keymap.set("n", "<leader>o", oil.open_float, { desc = "Open Oil Float" })
-    end,
+    opts = {
+        float = {
+            padding = 4,
+            border = "single",
+        },
+        confirmation = {
+            border = "single",
+        },
+        progress = {
+            border = "single",
+        },
+        ssh = {
+            border = "single",
+        },
+        keymaps_help = {
+            border = "single",
+        },
+    },
+    keys = {
+        {
+            "<leader>o",
+            function()
+                require("oil").open_float()
+            end,
+            desc = "Open Oil Float",
+        },
+    },
 }
