cde81867bb931c724197b97bca1f29ede4e06ec8
Philip Mørch <mrphil2105@gmail.com>
2025-11-25 16:09:35 +0100

    fix: remove unnecessary read of LUA_PATH and LUA_CPATH

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-require("config.env")
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
--- lua/config/env.lua
+++
@@ -1,4 +0,0 @@
-local lua_path = os.getenv("LUA_PATH")
-local lua_cpath = os.getenv("LUA_CPATH")
-package.cpath = package.cpath .. ";" .. lua_path
-package.path = package.path .. ";" .. lua_cpath
