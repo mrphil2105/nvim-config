75393af59510ee7dd80cdcb8db0dfd3fd14e86c5
Philip Mørch <mrphil2105@gmail.com>
2025-11-30 15:51:59 +0100

    refactor: use plugin spec opts field passed to config

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,43 +1,44 @@
+local utils = require("utils")
 return {
     "stevearc/conform.nvim",
-    config = function()
-        local utils = require("utils")
+    opts = {
+        formatters_by_ft = {
+            lua = { "stylua" },
+            python = { "black" },
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            cs = { "csharpier" },
+            java = { "clang-format" },
+            c = { "clang-format" },
+            cpp = { "clang-format" },
+            rust = { "rustfmt" },
+            xml = { "xstyler", "remove_bom" },
+            nix = { "nixfmt" },
+            css = { "cssbeautifier" },
+        },
+        formatters = {
+            xstyler = {
+                command = "xstyler",
+                args = { "--loglevel", "None", "--file", "$FILENAME" },
+                stdin = false,
+                condition = function(_, ctx) return utils.has_file_extension(ctx.filename, ".axaml") end,
+            },
+            remove_bom = {
+                command = "dos2unix",
+                args = { "$FILENAME" },
+                stdin = false,
+                condition = function(_, ctx) return utils.has_file_extension(ctx.filename, ".axaml") end,
+            },
+            cssbeautifier = {
+                command = "css-beautify",
+                args = { "--stdin" },
+                condition = function(_, ctx) return utils.has_file_extension(ctx.filename, ".css") end,
+            },
+        },
+    },
+    config = function(_, opts)
         local conform = require("conform")
-        conform.setup {
-            formatters_by_ft = {
-                lua = { "stylua" },
-                python = { "black" },
-                typescript = { "prettier" },
-                typescriptreact = { "prettier" },
-                cs = { "csharpier" },
-                java = { "clang-format" },
-                c = { "clang-format" },
-                cpp = { "clang-format" },
-                rust = { "rustfmt" },
-                xml = { "xstyler", "remove_bom" },
-                nix = { "nixfmt" },
-                css = { "cssbeautifier" },
-            },
-            formatters = {
-                xstyler = {
-                    command = "xstyler",
-                    args = { "--loglevel", "None", "--file", "$FILENAME" },
-                    stdin = false,
-                    condition = function(_, ctx) return utils.has_file_extension(ctx.filename, ".axaml") end,
-                },
-                remove_bom = {
-                    command = "dos2unix",
-                    args = { "$FILENAME" },
-                    stdin = false,
-                    condition = function(_, ctx) return utils.has_file_extension(ctx.filename, ".axaml") end,
-                },
-                cssbeautifier = {
-                    command = "css-beautify",
-                    args = { "--stdin" },
-                    condition = function(_, ctx) return utils.has_file_extension(ctx.filename, ".css") end,
-                },
-            },
-        }
+        conform.setup(opts)
         local group = vim.api.nvim_create_augroup("Formatter", {})
         vim.api.nvim_create_autocmd("BufWritePre", {
             group = group,
