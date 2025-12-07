691c39f5474e844a3cbdedd3475528104800c792
Philip Mørch <mrphil2105@gmail.com>
2025-12-07 18:50:11 +0100

    fix: exclude cmd field for LSP server settings

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,9 +1,11 @@
 local servers = require("config.lsp.servers")
 for server_name, settings in pairs(servers) do
+    local cmd = settings.cmd
+    settings.cmd = nil
     vim.lsp.config[server_name] = {
         settings = settings,
         filetypes = settings.filetypes,
-        cmd = settings.cmd,
+        cmd = cmd,
     }
     vim.lsp.enable(server_name)
 end
