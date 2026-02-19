0296271ff6ef5639a702dbcfb64b8b2425992a33
Philip Mørch <mrphil2105@gmail.com>
2026-02-19 15:37:41 +0100

    feat(nix): disable animations

--- home/hypr/hyprland.nix
+++ home/hypr/hyprland.nix
@@ -40,45 +40,13 @@ in
           render_power = 3;
           color = "rgba(1a1a1aee)";
         };
-        blur = {
-          enabled = false;
-        };
+        blur.enabled = false;
       };
-      animations = {
-        bezier = [
-          "easeOutQuint,0.23,1,0.32,1"
-          "easeInOutCubic,0.65,0.05,0.36,1"
-          "linear,0,0,1,1"
-          "almostLinear,0.5,0.5,0.75,1.0"
-          "quick,0.15,0,0.1,1"
-        ];
-        animation = [
-          "global, 1, 10, default"
-          "border, 1, 4, easeOutQuint"
-          "windows, 1, 2.5, easeOutQuint"
-          "windowsIn, 1, 2.5, easeOutQuint, popin 80%"
-          "windowsOut, 1, 2, linear, popin 80%"
-          "fadeIn, 1, 1.5, almostLinear"
-          "fadeOut, 1, 1.5, almostLinear"
-          "fade, 1, 3, quick"
-          "layers, 1, 3, easeOutQuint"
-          "layersIn, 1, 3, easeOutQuint, fade"
-          "layersOut, 1, 2.5, linear, fade"
-          "fadeLayersIn, 1, 2, almostLinear"
-          "fadeLayersOut, 1, 1.5, almostLinear"
-          "workspaces, 1, 2.5, easeOutQuint, slide"
-          "workspacesIn, 1, 2, easeOutQuint, slide"
-          "workspacesOut, 1, 2.5, easeOutQuint, slide"
-        ];
-      };
-      dwindle = {
-        pseudotile = true;
-        preserve_split = true;
-      };
-      misc = {
-        force_default_wallpaper = 1;
-        enable_anr_dialog = false;
-      };
+      animations.enabled = false;
+      dwindle.pseudotile = true;
+      dwindle.preserve_split = true;
+      misc.force_default_wallpaper = 1;
+      misc.enable_anr_dialog = false;
       input = {
         kb_layout = "us,dk";
         kb_options = "caps:escape,grp:win_space_toggle";
