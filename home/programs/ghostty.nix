16cd439fbdfdf5a07ff36b4f3787d22cbdffed43
Philip Mørch <mrphil2105@gmail.com>
2026-02-24 13:44:43 +0100

    chore(nix): avoid explicitly setting font for bold and italic

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -6,9 +6,6 @@
     window-padding-x = 0;
     window-padding-y = 0;
     font-family = "JetBrainsMonoNL NF";
-    font-family-bold = "JetBrainsMonoNL NF";
-    font-family-italic = "JetBrainsMonoNL NF";
-    font-family-bold-italic = "JetBrainsMonoNL NF";
     font-size = 12;
     link-previews = false;
     resize-overlay = "never";
