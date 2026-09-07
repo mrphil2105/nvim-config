0e731de7f4ee3b52ed6bb675a814974d68d5270c
Philip Mørch <mrphil2105@gmail.com>
2026-09-07 19:43:38 +0200

    fix(nix): use Lua version of DPMS dispatcher

--- modules/hypr/hypr.nix
+++ modules/hypr/hypr.nix
@@ -10,8 +10,8 @@
       settings.listener = [
         {
           timeout = 180;
-          on-timeout = "hyprctl dispatch dpms off";
-          on-resume = "hyprctl dispatch dpms on";
+          on-timeout = "hyprctl dispatch 'hl.dsp.dpms({ action = \"disable\" })'";
+          on-resume = "hyprctl dispatch 'hl.dsp.dpms({ action = \"enable\" })'";
         }
       ];
     };
